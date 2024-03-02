9a3934436ff8e10bf4fe050159bd6749d3e392f0
Ruchithrt <ruchithtilavalli3266@gmail.com>
2024-03-02 08:17:20 +0530

    all api

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,22 +1,37 @@
 import { User } from "@/models/user";
-import { connectDB } from "@/utils/features";
+import { connectDB, cookieSetter, generateToken } from "@/utils/features";
 const { asyncError, errorHandler } = require("@/middlewares/error");
+import bcrypt from "bcrypt";
 
 const handler = asyncError(async (req, res) => {
+  if (req.method !== "POST")
+    return errorHandler(res, 400, "Only POST Method is allowed");
+
   const { name, email, password } = req.body;
   if (!name || !email || !password)
     return errorHandler(res, 400, "Please enter all fields");
 
   await connectDB();
 
-  const user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
   if (user) return errorHandler(res, 400, "User already exists");
 
-  await User.create({
+  const hashpassword = await bcrypt.hash(password, 10);
+
+  user = await User.create({
     name,
     email,
-    password,
+    password: hashpassword,
+  });
+
+  const token = generateToken(user._id);
+
+  cookieSetter(res, token, true);
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
   });
 });
 
--- utils/features.js
+++ utils/features.js
@@ -1,9 +1,35 @@
 import mongoose from "mongoose";
-// import { serialize } from "cookie";
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/user";
+import { serialize } from "cookie";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
 
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
   console.log(`Database Connected on ${connection.host}`);
+};
+
+export const cookieSetter = (res, token, set) => {
+  res.setHeader(
+    "Set-Cookie",
+    serialize("token", set ? token : "", {
+      path: "/",
+      httpOnly: true,
+      maxAge: set ? 15 * 24 * 60 * 60 * 1000 : "", // 15 days
+    })
+  );
+};
+
+export const generateToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET);
+};
+
+export const checkAuth = async (req) => {
+  const cookie = req.headers.cookie;
+  if (!cookie) return null;
+
+  const token = cookie.split("=")[1]; // bcz cokkie is in format of "token = 'ewhbff' " to extract only token split at "=" and choose the second part (index 1 instead of 0)
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  return await User.findById(decoded._id);
 };
